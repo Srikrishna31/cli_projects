15ab06ceb2b03598641fcca8054ee9ecbed4b729
Krishna Addepalli <coolkrishna31@gmail.com>
2024-01-15 11:56:58 +0100

    Finished with echor command

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -1,25 +1,39 @@
 use assert_cmd::Command;
 use predicates::prelude::*;
 use std::fs;
+use rstest::rstest;
+
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
 #[test]
-fn dies_no_args() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+fn dies_no_args() -> TestResult{
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("USAGE"));
+
+    Ok(())
 }
 
 #[test]
-fn runs() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+fn runs() -> TestResult {
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.arg("hello").assert().success();
+    Ok(())
 }
 
+#[rstest]
+#[case(&["Hello there"], "tests/expected/hello1.txt")]
+#[case(&["Hello", "there"], "tests/expected/hello2.txt")]
+#[case(&["Hello   there", "-n"], "tests/expected/hello1.n.txt")]
+#[case(&["-n", "Hello", "there"], "tests/expected/hello2.n.txt")]
+fn run(#[case] args: &[&str], #[case] expected_file: &str) -> TestResult {
+    let expected = fs::read_to_string(expected_file)?;
+    Command::cargo_bin("echor")?
+        .args(args)
+        .assert()
+        .success()
+        .stdout(expected);
 
-#[test]
-fn hello1() {
-    let outfile = "tests/expected/hello1.txt";
-    let expected = fs::read_to_string(outfile).unwrap();
-    let mut cmd = Command::cargo_bin("echor").unwrap();
-    cmd.arg("Hello there").assert().success().stdout(expected);
+    Ok(())
 }
