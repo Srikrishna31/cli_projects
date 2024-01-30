62091a787c940aa5fea29856dfb25cfafe1dda11
Krishna Addepalli <coolkrishna31@gmail.com>
2024-01-30 08:23:51 +0100

    Write function to extract fields from csv files

--- cutr/src/data_extractor.rs
+++ cutr/src/data_extractor.rs
@@ -1,4 +1,6 @@
 use core::ops::Range;
+use csv::StringRecord;
+
 pub(super) fn extract_chars(line: &str, char_pos: &[Range<usize>]) -> String {
     let mut res = String::new();
     for r in char_pos {
@@ -34,12 +36,29 @@ pub(super) fn extract_bytes(line: &str, byte_pos: &[Range<usize>]) -> String {
     String::from_utf8_lossy(&res).to_string()
 }
 
-pub(super) fn extract_fields(line: &str, field_pos: &[Range<usize>]) -> String {
-    unimplemented!();
+pub(super) fn extract_fields(line: &StringRecord, field_pos: &[Range<usize>]) -> Vec<String> {
+    let mut res: Vec<String> = vec![];
+    for r in field_pos {
+        let mut lres: Vec<_> = line
+            .iter()
+            .enumerate()
+            .filter(|(i, _)| i >= &r.start)
+            .take_while(|(i, c)| i < &r.end)
+            .map(|(_, s)| s.to_string())
+            .filter(|s| !s.is_empty())
+            .collect();
+
+        if !lres.is_empty() {
+            res.append(&mut lres);
+        }
+    }
+
+    res
 }
 #[cfg(test)]
 mod unit_tests {
     use super::{extract_bytes, extract_chars, extract_fields};
+    use csv::StringRecord;
     #[test]
     fn test_extract_chars() {
         assert_eq!(extract_chars("", &[0..1]), "".to_string());
@@ -57,5 +76,15 @@ mod unit_tests {
         assert_eq!(extract_bytes("ábc", &[0..3]), "áb".to_string());
         assert_eq!(extract_bytes("ábc", &[0..4]), "ábc".to_string());
         assert_eq!(extract_bytes("ábc", &[3..4, 2..3]), "cb".to_string());
+    }
+
+    #[test]
+    fn test_extract_fields() {
+        let rec = StringRecord::from(vec!["Captain", "Sham", "12345"]);
+        assert_eq!(extract_fields(&rec, &[0..1]), &["Captain"]);
+        assert_eq!(extract_fields(&rec, &[1..2]), &["Sham"]);
+        assert_eq!(extract_fields(&rec, &[0..1, 2..3]), &["Captain", "12345"]);
+        assert_eq!(extract_fields(&rec, &[0..1, 3..4]), &["Captain"]);
+        assert_eq!(extract_fields(&rec, &[1..2, 0..1]), &["Sham", "Captain"]);
     }
 }
