d6af5da6e30afb900b3498c17b5db70df9e0b80a
Krishna Addepalli <coolkrishna31@gmail.com>
2024-02-05 11:58:44 +0100

    Add unit tests for parse_num function to be used to parse lines and bytes

--- tailr/src/lib.rs
+++ tailr/src/lib.rs
@@ -1,15 +1,134 @@
 use command_utils::{open, MyResult};
+use clap::{Arg, Command};
+
+#[derive(Debug, PartialEq)]
+enum TakeValue {
+    PlusZero,
+    TakeNum(i64),
+}
 
 #[derive(Debug)]
 pub struct Config {
-
+    files: Vec<String>,
+    bytes: Option<TakeValue>,
+    lines: TakeValue,
+    quiet: bool,
 }
 
 pub fn get_args() -> MyResult<Config> {
-    Ok(Config {})
+    let matches = Command::new("tailr")
+        .version("0.1.0")
+        .author("Krishna Addepalli <coolkrishna31@gmail.com>")
+        .about("Rust tail")
+        .arg(
+            Arg::new("files")
+                .value_name("FILE")
+                .help("Input files")
+                // .default_value("-")
+                .num_args(1..)
+                .required(true),
+        )
+        .arg(
+            Arg::new("bytes")
+                .value_name("BYTES")
+                .help("Number of bytes")
+                .short('c')
+                .long("bytes")
+                .num_args(1)
+                .required(false),
+        )
+        .arg(
+            Arg::new("lines")
+                .value_name("LINES")
+                .help("Number of lines")
+                .short('n')
+                .long("lines")
+                .num_args(1)
+                .required(false)
+                .conflicts_with("bytes"),
+        )
+        .arg(
+            Arg::new("quiet")
+                .help("Suppress headers")
+                .short('q')
+                .long("quiet")
+                .num_args(0)
+                .required(false),
+        )
+        .get_matches();
+
+    Ok(Config {
+        files: matches.get_many::<String>("files").unwrap().map(String::to_owned).collect::<Vec<String>>(),
+        bytes: None,
+        lines: TakeValue::PlusZero,
+        quiet: matches.get_flag("quiet"),
+    })
 }
 
+fn parse_num(val: &str) -> MyResult<TakeValue> {
+    unimplemented!();
+}
 pub fn run(config: Config) -> MyResult<()> {
     dbg!(&config);
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{parse_num, TakeValue};
+
+    #[test]
+    fn test_parse_num() {
+        // All integers should be interpreted as negative numbers
+        let res = parse_num("3");
+        assert!(res.is_ok());
+        assert_eq!(res, Ok(TakeValue::TakeNum(-3)));
+
+        // A leading "+" should result in a positive number
+        let res = parse_num("+3");
+        assert!(res.is_ok());
+        assert_eq!(res, Ok(TakeValue::TakeNum(3)));
+
+        // An explicit "-" value should result in a negative number
+        let res = parse_num("-3");
+        assert!(res.is_ok());
+        assert_eq!(res, Ok(TakeValue::TakeNum(-3)));
+
+        // Zero is zero
+        let res = parse_num("0");
+        assert!(res.is_ok());
+        assert_eq!(res, Ok(TakeValue::TakeNum(0)));
+
+        //Plus zero is special
+        let res = parse_num("+0");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), TakeValue::PlusZero);
+
+        // Test boundaries
+        let res = parse_num(&i64::MAX.to_string());
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), TakeValue::TakeNum(i64::MIN + 1));
+
+        let res = parse_num(&(i64::MIN + 1).to_string());
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), TakeValue::TakeNum(i64::MIN + 1));
+
+        let res = parse_num(&format!("+{}", i64::MAX));
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), TakeValue::TakeNum(i64::MAX));
+
+        let res = parse_num(&i64::MIN.to_string());
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), TakeValue::TakeNum(i64::MIN));
+
+        // A floating-point value is invalid
+        let res = parse_num("3.14");
+        assert!(res.is_err());
+        assert_eq!(res, Err("Invalid number: 3.14"));
+
+        // Any noninteger string is invalid
+        let res = parse_num("foo");
+        assert!(res.is_err());
+        assert_eq!(res, Err("Invalid number: foo"));
+    }
 }
