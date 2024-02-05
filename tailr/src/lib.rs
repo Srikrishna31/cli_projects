d89b5dfe21743efea36e48648cd6d30fee1641db
Krishna Addepalli <coolkrishna31@gmail.com>
2024-02-05 10:01:45 +0100

    Add initial files for tailr command

---
+++ tailr/src/lib.rs
@@ -0,0 +1,15 @@
+use command_utils::{open, MyResult};
+
+#[derive(Debug)]
+pub struct Config {
+
+}
+
+pub fn get_args() -> MyResult<Config> {
+    Ok(Config {})
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    dbg!(&config);
+    Ok(())
+}
