a3cb006a3007bdddd7185d9db89485743794ecbb
Krishna Addepalli <coolkrishna31@gmail.com>
2024-02-08 11:09:50 +0100

    Correctly parse the command line arguments

--- fortuner/src/lib.rs
+++ fortuner/src/lib.rs
@@ -16,43 +16,89 @@ pub fn get_args() -> MyResult<Config> {
         .about("Rust fortune")
         .arg(
             Arg::new("sources")
-                .value_name("SOURCE")
+                .value_name("FILE")
                 .help("Fortune sources")
                 .default_value("fortune")
-                .num_args(0..),
+                .num_args(1..)
+                .required(true),
         )
         .arg(
             Arg::new("pattern")
                 .value_name("PATTERN")
                 .help("Pattern")
-                .short('p')
+                .short('m')
                 .long("pattern")
                 .num_args(1),
         )
         .arg(
             Arg::new("seed")
                 .value_name("SEED")
-                .help("Seed")
+                .help("Random seed")
                 .short('s')
                 .long("seed")
                 .num_args(1),
+        )
+        .arg(
+            Arg::new("insensitive")
+                .help("Case-insensitive pattern matching")
+                .short('i')
+                .long("insensitive")
+                .num_args(0),
         )
         .get_matches();
 
     let pattern = matches
         .get_one::<String>("pattern")
-        .map(|p| RegexBuilder::new(p.as_str()).case_insensitive(true).build())
-        .transpose()
-        .map_err(|e| format!("-- pattern \"{e}\" is not a valid regex"))?;
+        .map(|p| {
+            RegexBuilder::new(p.as_str())
+                .case_insensitive(matches.get_flag("insensitive"))
+                .build()
+                .map_err(|e| format!("Invalid --pattern \"{e}\""))
+        })
+        .transpose()?;
 
     Ok(Config {
-        sources: matches.get_many::<String>("sources").unwrap().map(|s| s.to_owned()).collect(),
+        sources: matches
+            .get_many::<String>("sources")
+            .unwrap()
+            .map(|s| s.to_owned())
+            .collect(),
         pattern,
-        seed: Some(0),
+        seed: matches
+            .get_one::<String>("seed")
+            .map(|s| parse_u64(s.as_str()))
+            .transpose()?,
     })
 }
 
 pub fn run(config: Config) -> MyResult<()> {
     dbg!(config);
     Ok(())
+}
+
+fn parse_u64(val: &str) -> MyResult<u64> {
+    match val.parse() {
+        Ok(n) => Ok(n),
+        _ => Err(From::from(format!("\"{val}\" not a valid integer"))),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::parse_u64;
+
+    #[test]
+    fn test_parse_u64() {
+        let res = parse_u64("a");
+        assert!(res.is_err());
+        assert_eq!(res.unwrap_err().to_string(), "\"a\" not a valid integer");
+
+        let res = parse_u64("0");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), 0);
+
+        let res = parse_u64("4");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), 4);
+    }
 }
