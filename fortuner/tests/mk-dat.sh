be2d053e8a7b1f63317297ec71a42d84495210af
Krishna Addepalli <coolkrishna31@gmail.com>
2024-02-08 09:33:33 +0100

    Add test inputs and expected output files for fortuner command

---
+++ fortuner/tests/mk-dat.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+DIR="./tests/inputs"
+
+cd $DIR
+rm -f *.dat
+
+for FILE in *; do
+  if [[ -f $FILE ]]; then
+    echo "Creating $FILE.dat"
+    strfile -c % $FILE $FILE.dat > /dev/null
+  fi
+done
+
+echo "Done."
