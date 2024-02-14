3eda529befe81643b9f5a8060128c9d0b3769329
Krishna Addepalli <coolkrishna31@gmail.com>
2024-02-14 12:57:45 +0100

    Add initial project files for lsr command

---
+++ lsr/src/lib.rs
@@ -0,0 +1,33 @@
+use clap::{Command, Arg};
+use command_utils::{open, MyResult};
+
+#[derive(Debug)]
+pub struct Config {
+    paths: Vec<String>,
+    long: bool,
+    show_hidden: bool,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = Command::new("lsr")
+        .version("0.1.0")
+        .author("Krishna Addepalli <coolkrishna31@gmail.com>")
+        .about("Rust ls")
+        .arg(
+            Arg::new("all")
+                .help("Show hidden files")
+                .short('a')
+                .long("all")
+                .num_args(0),
+        );
+    Ok(Config {
+        paths: vec![],
+        long: false,
+        show_hidden: false,
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    dbg!(config);
+    Ok(())
+}
