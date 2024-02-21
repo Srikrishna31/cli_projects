988106dd9e44bf6821fbb1ae19d36b15de6a4c44
Krishna Addepalli <coolkrishna31@gmail.com>
2024-02-21 13:13:25 +0100

    Add function for formatting modes into string

--- lsr/src/lib.rs
+++ lsr/src/lib.rs
@@ -1,7 +1,10 @@
+use chrono::Local;
 use clap::{Arg, Command};
 use command_utils::{open, MyResult};
 use std::fs;
+use std::os::unix::fs::MetadataExt;
 use std::path::PathBuf;
+use tabular::{Row, Table};
 
 #[derive(Debug)]
 pub struct Config {
@@ -83,9 +86,87 @@ fn find_dir_entries(paths: &[String], show_hidden: bool) -> MyResult<Vec<PathBuf
     Ok(entries)
 }
 
+fn format_output(paths: &[PathBuf]) -> MyResult<String> {
+    //                 1  2    3    4    5    6    7    8
+    let fmt = "{:<}{:<} {:>} {:<} {:<} {:>} {:<} {:<}";
+    let mut table = Table::new(fmt);
+
+    for path in paths {
+        table.add_row(
+            Row::new()
+                .with_cell(path.metadata().map_or("-".to_string(), |m| {
+                    if m.is_dir() {
+                        "d".to_string()
+                    } else {
+                        "-".to_string()
+                    }
+                }))
+                .with_cell(
+                    path.metadata()
+                        .map_or("?".to_string(), |m| format_mode(m.mode()).to_string()),
+                )
+                .with_cell(path.metadata().map_or(0, |m| m.nlink()))
+                .with_cell(path.metadata().map_or("?".to_string(), |m| {
+                    users::get_user_by_uid(m.uid())
+                        .unwrap()
+                        .name()
+                        .to_string_lossy()
+                        .to_string()
+                }))
+                .with_cell(path.metadata().map_or("?".to_string(), |m| {
+                    users::get_group_by_gid(m.gid())
+                        .unwrap()
+                        .name()
+                        .to_string_lossy()
+                        .to_string()
+                }))
+                .with_cell(
+                    path.metadata()
+                        .map_or("?".to_string(), |m| m.len().to_string()),
+                )
+                .with_cell(path.metadata().map_or("?".to_string(), |m| {
+                    chrono::DateTime::<Local>::from(m.modified().unwrap())
+                        .format("%b %e %y %H:%M")
+                        .to_string()
+                }))
+                .with_cell(path.metadata().map_or("?".to_string(), |_| {
+                    path.file_name().unwrap().to_string_lossy().to_string()
+                })),
+        );
+    }
+
+    Ok(format!("{}", table))
+}
+
+// Given a file mode in octal format like 0o755, return a string like "rwxr-xr-x"
+fn format_mode(mode: u32) -> String {
+    let mut s = String::with_capacity(9);
+    for (i, (perm, c)) in [
+        (0o400, 'r'),
+        (0o200, 'w'),
+        (0o100, 'x'),
+        (0o040, 'r'),
+        (0o020, 'w'),
+        (0o010, 'x'),
+        (0o004, 'r'),
+        (0o002, 'w'),
+        (0o001, 'x'),
+    ]
+    .iter()
+    .enumerate()
+    {
+        if mode & perm != 0 {
+            s.push(*c);
+        } else {
+            s.push('-');
+        }
+    }
+    s
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::find_dir_entries;
+    use crate::{find_dir_entries, format_mode};
 
     #[test]
     fn test_find_dir_entries() {
@@ -158,5 +239,13 @@ mod tests {
             filenames,
             ["tests/inputs/bustle.txt", "tests/inputs/dir/spiders.txt"]
         );
+    }
+
+    #[test]
+    fn test_format_mode() {
+        assert_eq!(format_mode(0o755), "rwxr-xr-x");
+        assert_eq!(format_mode(0o644), "rw-r--r--");
+        assert_eq!(format_mode(0o421), "r---w---x");
+        assert_eq!(format_mode(0o777), "rwxrwxrwx");
     }
 }
